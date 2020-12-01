bb21e55e3ff53896505c11fe4671243e71b0a663
Volffied <70651519+Volffied@users.noreply.github.com>
2020-12-01 15:45:34 +0700

    history model controller

--- app/Model/HorderModel.php
+++ app/Model/HorderModel.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class HorderModel extends Model
 {
@@ -12,5 +13,16 @@ class HorderModel extends Model
     public $timestamps      = true;
     protected $fillable     = ['tanggal_trans','subtotal','grandtotal','metode_pembayaran','kode_customer','kode_promo'];
 
-
+    public function getAllDataBy($id){
+        $query = HorderModel::select(["horder.*","dorder.*"])
+                            ->join("dorder","kode_horder","id_horder")
+                            ->where("horder.kode_customer",$id)
+                            ->get();
+        return $query;
+    }
+    public function history(){
+        return $this->belongsToMany(BarangModel::class,'dorder','kode_horder','kode_barang')
+                    ->withPivot("qty","total")
+                    ->as("dorder");
+    }
 }
